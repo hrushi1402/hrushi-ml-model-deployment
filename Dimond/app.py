f97adedd60d7340ac8cc2d1e389d4bd0df28b9db
Hrushikesh Gadade <125385973+hrushi1402@users.noreply.github.com>
2024-04-04 16:19:26 +0530

    Update app.py

--- Dimond/app.py
+++ Dimond/app.py
@@ -18,25 +18,13 @@ color_n = {'J':7,'I':6,'H':5,'G':4,'F':3,'E':2,'D':1}
 clarity_n = {'I1':8,'SI2':7,'SI1':6,'VS2':5,'VS1':4,'VVS2':3,'VVS1':2,'IF':1}
 
 # Function to preprocess input data
-def preprocess_data(carat, cut, color, clarity, depth, table, price, x, y, z):
+def preprocess_data(carat, cut, color, clarity, depth, table, x, y, z):
     # Map categorical variables to numerical values
     cut_encoded = cut_n.get(cut, 0)  # Use 0 as default value if cut not found
     color_encoded = color_n.get(color, 0)  # Use 0 as default value if color not found
     clarity_encoded = clarity_n.get(clarity, 0)  # Use 0 as default value if clarity not found
     
-    # Create DataFrame with preprocessed data
-    data = pd.DataFrame({
-        'carat': [carat],
-        'cut': [cut_encoded],
-        'color': [color_encoded],
-        'clarity': [clarity_encoded],
-        'depth': [depth],
-        'table': [table],
-       # 'price': [price],
-        'x': [x],
-        'y': [y],
-        'z': [z]
-    })
+  
     
     return data
 # Creating buttons
