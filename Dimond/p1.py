9b5752d415972d7e1fcc432df9a3f4819f028785
Hrushikesh Gadade <125385973+hrushi1402@users.noreply.github.com>
2024-04-05 20:25:32 +0530

    Create p1.py
    practice app

---
+++ Dimond/p1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+# horizontal Menu
+selected2 = option_menu(None, ["Home", "ML Models", "Portfolio"], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+    selected2
