c02ace4856f3ed2e664c11bcc5f2c1bc91925b87
Hrushikesh Gadade <125385973+hrushi1402@users.noreply.github.com>
2024-04-05 20:32:21 +0530

    Update p1.py

--- Dimond/p1.py
+++ Dimond/p1.py
@@ -2,10 +2,20 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected = option_menu("Main Menu", ['Diamond price Prediction',
+                             'Diabetes Prediction',
+                             'Parkinsons Prediction'], 
+         default_index=1)
     selected
 
+# sidebar for navigation
+with st.sidebar:
+    selected = st.selectbox('Model Prediction System',
+                            ['Diamond price Prediction',
+                             'Diabetes Prediction',
+                             'Parkinsons Prediction'],
+                            index=0,
+                            help='Select a model for prediction')
 # horizontal Menu
 selected2 = option_menu(None, ["Home", "ML Models", "Portfolio"], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
